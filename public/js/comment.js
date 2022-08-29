6586bd2823e2e909ee7ce9b31ca63bf5b2adb8ae
JVelezFD <jvelez117@gmail.com>
2022-08-29 15:39:54 -0400

    added singlepost handlebar for comment route

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,11 +1,9 @@
 
 
-
-
 async function commentFormHandler(event) {
     event.preventDefault();
 
-    const comment_text = document.querySelector('input[name="comment-body"]').value.trim();
+    const comment_text = document.querySelector("#comment").value;
 
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1];
@@ -32,4 +30,4 @@ async function commentFormHandler(event) {
     }
 }
 
-document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+document.querySelector('#comment-form').addEventListener('submit', commentFormHandler);
