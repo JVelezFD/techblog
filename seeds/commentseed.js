a49dca4e2aa1cb4c5a80b51b24dd31f87e758e86
JVelezFD <jvelez117@gmail.com>
2022-09-02 09:12:53 -0400

    cleaing seed data

--- seeds/commentseed.js
+++ seeds/commentseed.js
@@ -10,11 +10,7 @@ const commentData = [{
         user_id: 2,
         post_id: 2
     },
-    {
-        comment_text: " Tester sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-        user_id: 3,
-        post_id: 3
-    }
+   
 ];
 
 const seedComments = () => Comment.bulkCreate(commentData);
